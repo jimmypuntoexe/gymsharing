1dd06928198793e917696441783af7eabeefd41c
Andrea Carubelli <a.carubelli@campus.unimib.it>
2019-12-13 16:35:56 +0100

    Add BasicController.java, infoGym.html and modified GymController.java and gyms.html

--- gymsharingapp/src/main/java/com/example/assignment3/Controller/GymController.java
+++ gymsharingapp/src/main/java/com/example/assignment3/Controller/GymController.java
@@ -6,6 +6,7 @@ import com.example.assignment3.Repository.GymRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +18,27 @@ public class GymController {
   @Autowired
   GymRepository repository;
 
-  @RequestMapping("/")
-  public String home() {
-    return "index";
-  }
+  // @RequestMapping("/")
+  // public String home() {
+  //   return "index";
+  // }
 
   @RequestMapping("/insertGym")
   public String insertGym() {
     return "insertGym";
   }
+
+  @RequestMapping(value="/deleteGym/{id}", method=RequestMethod.GET)
+	public String gymDelete(@PathVariable Long id) {
+       repository.delete(id);
+       return "redirect:/gyms/";
+  }
+  
+  @RequestMapping("/gym/{id}")
+	public String gym(@PathVariable Long id, Model model) {
+        model.addAttribute("gym", repository.findOne(id));
+        return "infoGym";
+	}
 
   @RequestMapping(value="/gyms", method=RequestMethod.GET)
 	public String gymList(Model model) {
