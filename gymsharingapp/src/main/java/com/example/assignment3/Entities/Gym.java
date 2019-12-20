3a9d102cee600dbd0e06f6d50e8a9fc54bbcbdc0
Andrea Carubelli <a.carubelli@campus.unimib.it>
2019-12-20 13:31:59 +0100

    Add some features but not complete connect subscriptions-user

--- gymsharingapp/src/main/java/com/example/assignment3/Entities/Gym.java
+++ gymsharingapp/src/main/java/com/example/assignment3/Entities/Gym.java
@@ -2,6 +2,7 @@ package com.example.assignment3.Entities;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -29,7 +30,7 @@ public class Gym {
 	// @JsonBackReference
 	// private List<Gym> affiliateGym;
 
-	@ManyToMany
+	@ManyToMany(cascade = CascadeType.ALL)
 	private List<PersonalTrainer> personalTrainers;
 
 	@OneToMany
