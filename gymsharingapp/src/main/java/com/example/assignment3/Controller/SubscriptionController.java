c188600698db2ab53de14f21169e27a6b58dcf8a
lapuile <lapuile@localhost.localdomain>
2019-12-20 18:50:43 +0100

    add edit and remove for SubscriptionController

--- gymsharingapp/src/main/java/com/example/assignment3/Controller/SubscriptionController.java
+++ gymsharingapp/src/main/java/com/example/assignment3/Controller/SubscriptionController.java
@@ -32,6 +32,8 @@ public class SubscriptionController {
 
     @RequestMapping("/gym/{id}/insertSubscriptions")
     public String subscriptionsGym(@PathVariable Long id, Model model) {
+        model.addAttribute("action", "insert");
+        model.addAttribute("sub", subRepository.findOne(id));
         model.addAttribute("gym", gymRepository.findOne(id));
         return "insertSubscriptions";
     }
@@ -71,6 +73,49 @@ public class SubscriptionController {
         Subscription sub = subRepository.findOne(idSub);
         gym.getSubscriptions().remove(sub);
         subRepository.delete(sub.getId());
+        return "redirect:/gym/" + gym.getId();
+    }
+
+    @RequestMapping("/gym/{idGym}/{idSub}/editSubscriptions")
+    public String editSubscriptionsGym(@PathVariable Long idGym, @PathVariable Long idSub,
+     Model model) {
+        Gym gym = gymRepository.findOne(idGym);
+        Subscription sub = subRepository.findOne(idSub);
+        model.addAttribute("action", "edit");
+        model.addAttribute("classType", subRepository.findOne(idSub).getClass().getSimpleName());
+        model.addAttribute("sub", sub); 
+        model.addAttribute("gym", gym);
+        //return "redirect:/gym/" + gym.getId()+ "/" + sub.getId() +"/insertSubscriptions";
+        return "insertSubscriptions";
+    }
+
+    @RequestMapping(value="/gym/{idGym}/{idSub}/updateSubscriptions", method=RequestMethod.GET)
+    public String updateSubscription(@PathVariable Long idGym, @PathVariable Long idSub, Model model, 
+    @RequestParam(required = false) String annualPrice,
+    @RequestParam(required = false) String monthPrice,
+    @RequestParam(required = false) String trialPrice) {
+        Gym gym = gymRepository.findOne(idGym);
+        String classType = subRepository.findOne(idSub).getClass().getSimpleName();
+        switch(classType){
+            case "AnnualSubscription":
+            Subscription annualSubscription = subRepository.findOne(idSub);
+            ((AnnualSubscription) annualSubscription).setPrice(annualPrice);
+            subRepository.save(annualSubscription);
+            break;
+            case "MonthSubscription":
+            Subscription monthSubscription = subRepository.findOne(idSub);
+            ((MonthSubscription) monthSubscription).setPrice(monthPrice);
+            subRepository.save(monthSubscription);
+            break;
+            case "TrialSubscription":
+            Subscription trialSubscription = subRepository.findOne(idSub);
+            ((TrialSubscription) trialSubscription).setPrice(trialPrice);
+            subRepository.save(trialSubscription);
+            break;
+        }
+        model.addAttribute("gym", gym);
+        model.addAttribute("sub", subRepository.findOne(idSub));
+
         return "redirect:/gym/" + gym.getId();
     }
 
