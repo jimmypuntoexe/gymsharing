2bcb0927a7c6271967cf37ce29e36dedf55502d9
lapuile <lapuile@localhost.localdomain>
2019-12-23 17:45:18 +0100

    add function buy subscription for users

--- gymsharingapp/src/main/java/com/example/assignment3/Assignment3Application.java
+++ gymsharingapp/src/main/java/com/example/assignment3/Assignment3Application.java
@@ -4,10 +4,16 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
-public class Assignment3Application {
+public class Assignment3Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment3Application.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
 
 }
