06949825692183bfa400f4aaf0617e2d1b9076ae
Andrea Carubelli <a.carubelli@campus.unimib.it>
2020-01-10 17:49:19 +0100

    remove a import in Gym.java

--- gymsharingapp/src/main/java/com/example/assignment3/Entities/Gym.java
+++ gymsharingapp/src/main/java/com/example/assignment3/Entities/Gym.java
@@ -3,7 +3,6 @@ package com.example.assignment3.Entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
