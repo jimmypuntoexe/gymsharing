36e17b0aae710af04909d128be7ec1c3906cf8e1
Andrea Carubelli <a.carubelli@campus.unimib.it>
2020-01-12 16:04:16 +0100

    fix bug in update user

--- gymsharingapp/src/main/java/com/example/assignment3/Controller/UserController.java
+++ gymsharingapp/src/main/java/com/example/assignment3/Controller/UserController.java
@@ -122,6 +122,7 @@ public class UserController {
         String regex = "^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$";
         User user = userRepository.findOne(id);
         user.setName(name);
+        user.setName(surname);
         if(birthDate.matches(regex)){
           user.setBirthDate(birthDate);
         }
